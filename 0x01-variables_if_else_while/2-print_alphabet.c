a879e04b799befc1da42c77789e1b520040b8bc4
timi0624 <kayode0624@gmail.com>
2023-09-10 07:05:33 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main functions
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	puthcar('\n');
+	return (0);
+}
