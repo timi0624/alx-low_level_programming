8446bf92aef5d145a2138dc0dd1ca99020cf0ddc
timi0624 <kayode0624@gmail.com>
2023-10-05 09:46:38 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper -  looks for letter cases
+ * @c: function parameter
+ * Return: 1 or 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
